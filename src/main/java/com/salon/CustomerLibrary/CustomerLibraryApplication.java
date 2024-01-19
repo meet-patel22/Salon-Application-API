aaff34b4214b180fd4cf108f5e851390d96d8730
meet-patel22 <meetpatidar1998@gmail.com>
2024-01-19 14:27:41 -0500

    1st

---
+++ src/main/java/com/salon/CustomerLibrary/CustomerLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.salon.CustomerLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerLibraryApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/salon/CustomerLibrary/Entity/Customer.java
@@ -0,0 +1,93 @@
+package com.salon.CustomerLibrary.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter 
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id ;
+	private String firstName ;
+	private String lastName ;
+	private int age ; 
+	private String gender ;
+	private String email ;
+	private String address ;
+	private int phoneNumber ;
+	
+		
+	
+	@Override
+	public String toString() {
+		return "Customer [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", gender=" + gender
+				+ ", email=" + email + ", address=" + address + ", phoneNumber=" + phoneNumber + "]";
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+	
+	
+	
+
+}
